a8395275e96b7772661858f46fec3d5a5d660e24
dennismachu <dennis.machu@outlook.com>
2021-03-29 10:27:10 +0000

    Query for client portfolio

--- src/main/java/com/ClientConnectivity/tradeSystem/DTOs/Portfolio.java
+++ src/main/java/com/ClientConnectivity/tradeSystem/DTOs/Portfolio.java
@@ -12,5 +12,29 @@ public class Portfolio {
     @Column(nullable = false, updatable = false)
     private Long ID;
     private String name;
+    private Long client_id ;
 
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(Long client_id) {
+        this.client_id = client_id;
+    }
 }
